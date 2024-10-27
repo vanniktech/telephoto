621587ed947d20944f9c51477a541f90eaf13a6c
Saket Narayan <saket@saket.me>
2024-10-26 23:39:14 -0400

    Remove ImageRegionDecoder#imageOrientation

--- zoomable-image/sub-sampling-image/src/test/kotlin/me/saket/telephoto/subsamplingimage/internal/BitmapCacheTest.kt
+++ zoomable-image/sub-sampling-image/src/test/kotlin/me/saket/telephoto/subsamplingimage/internal/BitmapCacheTest.kt
@@ -172,7 +172,6 @@ class BitmapCacheTest {
 
 private class FakeImageRegionDecoder : ImageRegionDecoder {
   override val imageSize: IntSize get() = error("unused")
-  override val imageOrientation: ExifMetadata.ImageOrientation get() = error("unused")
   val requestedRegions = MutableSharedFlow<ImageRegionTile>()
   val decodedBitmaps = Channel<ImageBitmap>()
 

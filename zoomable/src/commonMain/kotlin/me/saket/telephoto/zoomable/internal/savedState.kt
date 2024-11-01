e8e35c65b51bbcc5cb5c940b47c5f01960f96094
Saket Narayan <saket@saket.me>
2024-11-01 17:36:26 -0400

    Save x and y offsets in a single long

--- zoomable/src/commonMain/kotlin/me/saket/telephoto/zoomable/internal/savedState.kt
+++ zoomable/src/commonMain/kotlin/me/saket/telephoto/zoomable/internal/savedState.kt
@@ -1,40 +1,37 @@
 package me.saket.telephoto.zoomable.internal
 
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.util.packFloats
+import androidx.compose.ui.util.unpackFloat1
+import androidx.compose.ui.util.unpackFloat2
 import me.saket.telephoto.zoomable.GestureState
 import me.saket.telephoto.zoomable.UserZoomFactor
 
 @AndroidParcelize
 @Suppress("DataClassPrivateConstructor")
 internal data class ZoomableSavedState private constructor(
-  private val offsetX: Float?,
-  private val offsetY: Float?,
-  private val centroidX: Float?,
-  private val centroidY: Float?,
+  private val offset: Long?,
+  private val centroid: Long?,
   private val userZoom: Float?,
 ) : AndroidParcelable {
 
   constructor(gestureState: GestureState?) : this(
-    offsetX = gestureState?.offset?.x,
-    offsetY = gestureState?.offset?.y,
-    centroidX = gestureState?.lastCentroid?.x,
-    centroidY = gestureState?.lastCentroid?.y,
+    offset = gestureState?.offset?.packToLong(),
+    centroid = gestureState?.lastCentroid?.packToLong(),
     userZoom = gestureState?.userZoom?.value,
   )
 
   fun asGestureState(): GestureState? {
     return GestureState(
-      offset = Offset(
-        x = offsetX ?: return null,
-        y = offsetY ?: return null
-      ),
-      userZoom = UserZoomFactor(
-        value = userZoom ?: return null
-      ),
-      lastCentroid = Offset(
-        x = centroidX ?: return null,
-        y = centroidY ?: return null,
-      ),
+      offset = offset?.unpackAsOffset() ?: return null,
+      lastCentroid = centroid?.unpackAsOffset() ?: return null,
+      userZoom = UserZoomFactor(value = userZoom ?: return null),
     )
   }
 }
+
+private fun Offset.packToLong(): Long =
+  packFloats(x, y)
+
+private fun Long.unpackAsOffset(): Offset =
+  Offset(x = unpackFloat1(this), y = unpackFloat2(this))

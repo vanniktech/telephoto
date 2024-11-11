226cc73507d96410bc678c842ed7e7156730ce12
Saket Narayan <saket@saket.me>
2024-11-11 10:19:51 -0500

    Clarify that contentSize remains unaffected by zoom & pan

--- zoomable/src/commonMain/kotlin/me/saket/telephoto/zoomable/ZoomableContentTransformation.kt
+++ zoomable/src/commonMain/kotlin/me/saket/telephoto/zoomable/ZoomableContentTransformation.kt
@@ -31,8 +31,9 @@ interface ZoomableContentTransformation {
 
   /**
    * The size of the zoomable content that is currently known to [Modifier.zoomable].
-   * This is calculated using the value given to [ZoomableState.setContentLocation].
-   * Useful for synchronizing other elements with the zoomable content.
+   * This is calculated using the value given to [ZoomableState.setContentLocation] and
+   * does not account for the zoom and pan transformations. Useful for synchronizing
+   * other elements with the zoomable content.
    */
   val contentSize: Size
 
